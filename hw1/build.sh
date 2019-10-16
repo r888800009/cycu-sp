aa202f9e3cce1245e77e6bf6694c92974a4aeeca
r888800009 <r888800009@gmail.com>
2019-10-16 21:13:37 +0800

    Add file checker

--- hw1/build.sh
+++ hw1/build.sh
@@ -5,6 +5,19 @@ if [ -z $1]; then
   g++ *.cpp -D DEBUGING -g
   echo test
   ./a.out --debuging
+
+  echo test files
+  fileList='error1.txt test1.txt test2.txt'
+  rm test-temp -r
+  mkdir test-temp
+  for filename in $fileList; do
+    ./a.out ./TestFile/$filename > test-temp/$filename
+    drophead=12
+    diff <(tail -n +$drophead test-temp/$filename) <(tail -n +$drophead output/$filename)
+    if [ $? -eq 1 ]; then
+      echo $filename error
+    fi
+  done
 else
   echo Release mode
   g++ *.cpp -D NDEBUG
