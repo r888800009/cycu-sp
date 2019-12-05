6f57f75f01f1d580334673539648265b24ee3d53
r888800009 <r888800009@gmail.com>
2019-12-06 03:12:35 +0800

    Update build.sh

--- hw2/build.sh
+++ hw2/build.sh
@@ -6,6 +6,10 @@ if [ -z $1]; then
   g++ -J 4 *.cpp -D DEBUGING -g
   echo test
   ./a.out --debuging
+  cat fileTest/testList.txt|./a.out
+  diff -r fileTest fileTestPre
+  echo get no test
+  diff -r fileTest fileTestPre | grep '_output.txt'
 else
   echo Release mode
   g++ *.cpp -D NDEBUG
