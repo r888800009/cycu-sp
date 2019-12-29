4f5f6fd02b995fd5c60fb6cf8f0f25752c72574f
r888800009 <r888800009@gmail.com>
2019-12-29 20:09:10 +0800

    Update test.sh

--- hw3/test.sh
+++ hw3/test.sh
@@ -4,7 +4,13 @@
 
 rm tmpout -r
 mkdir tmpout
+
 cp $(find testfile -name '*.txt') tmpout
+for f in tmpout/input00*.txt; do
+  iconv -f UTF-8 -t BIG-5 $f > $f.tmp
+  mv $f.tmp $f
+done
+
 
 echo $(find tmpout -name '*.txt') | ./a.out > /dev/null
 
