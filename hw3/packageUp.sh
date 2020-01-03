f4ba3dc9a321908c4a5a1be245e3d711a5002fa5
r888800009 <r888800009@gmail.com>
2020-01-03 18:36:03 +0800

    Add auto packup.sh

---
+++ hw3/packageUp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+name="10612150"
+
+rm $name -r
+rm $name.zip
+pandoc README.md -o README.html
+pandoc README.md -o README.txt
+mkdir $name
+cp -r * $name
+
+zip -rl $name.zip $(cat list.txt |xargs -I % echo $name/%)
